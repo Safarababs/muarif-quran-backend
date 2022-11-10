f91811404bb118ef58772a409a065035819143aa
Safar Abbase <safarabbas73.sa@gmail.comm>
2022-11-10 04:35:08 +0300

    some change

--- index.js
+++ index.js
@@ -4,7 +4,21 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const app = express();
 
-app.use(cors());
+//Cors Configuration - Start
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested, Content-Type, Accept Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "POST, PUT, PATCH, GET, DELETE");
+    return res.status(200).json({});
+  }
+  next();
+});
+//Cors Configuration - End
+
 app.use(express.json());
 app.use(express.static(__dirname + "/public"));
 
