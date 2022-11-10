0991668483acbaf05098dd4df0919931cef0a2bf
Safar Abbase <safarabbas73.sa@gmail.comm>
2022-11-10 10:24:28 +0300

    change Db

--- routes/lectureRoute.js
+++ routes/lectureRoute.js
@@ -1,34 +1,26 @@
-const express =   require("express");
+const express = require("express");
 const router = express.Router();
 const Lecture = require("../models/lectureModel");
 
+router.route("/create").post((req, res) => {
+  const title = req.body.title;
+  const content = req.body.content;
+  const link = req.body.link;
+  const courseName = req.body.courseName;
 
-router.route("/create").post((req, res) =>{
-    const title = req.body.title;
-    const content = req.body.content;
-    const link = req.body.link;
-    const courseName = req.body.courseName
+  const newLecture = new Lecture({
+    title,
+    content,
+    link,
+    courseName,
+  });
 
-    const newLecture = new Lecture({
-        title,
-        content,
-        link,
-        courseName
-    });
-
-    newLecture.save();
+  newLecture.save();
 });
 
-
-
-router.route("/lectures").get((req, res) =>{
-    Lecture.find()
-        .then(foundLecture => res.json(foundLecture))
-})
-
-
-
-
-
+router.route("/lectures").get((req, res) => {
+  Lecture.find().then((foundLecture) => res.json(foundLecture));
+  res.send(foundLecture);
+});
 
 module.exports = router;
