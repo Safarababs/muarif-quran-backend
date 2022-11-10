152ecb2806da5c32f40e43a8f38bd268a6e3ee2f
Safar Abbase <safarabbas73.sa@gmail.comm>
2022-11-10 05:16:47 +0300

    some change

--- index.js
+++ index.js
@@ -4,24 +4,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const app = express();
 
-//Cors Configuration - Start
-app.use((req, res, next) => {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://muarif-quran.netlify.app/"
-  );
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested, Content-Type, Accept Authorization"
-  );
-  if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "POST, PUT, PATCH, GET, DELETE");
-    return res.status(200).json({});
-  }
-  next();
-});
-//Cors Configuration - End
-
+app.use(cors());
 app.use(express.json());
 app.use(express.static(__dirname + "/public"));
 
